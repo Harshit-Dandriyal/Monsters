2975e2f94e64b234a282619b3f23ff9a0903e442
Harshit-Dandriyal <dandriyalh9318@gmail.com>
2020-03-20 02:09:23 +0530

    Updates

---
+++ precache-manifest.92dc81446efc9f54d0f4458948425fac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3167547be2d30a6c3f421bbec6ab1ba0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a604e182f8311f3af21",
+    "url": "/static/css/main.72c9e53a.chunk.css"
+  },
+  {
+    "revision": "42341dbba133cf867170",
+    "url": "/static/js/2.e90063d3.chunk.js"
+  },
+  {
+    "revision": "5493ac8c9e221eeba3bec532634c6faf",
+    "url": "/static/js/2.e90063d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a604e182f8311f3af21",
+    "url": "/static/js/main.7ba94f49.chunk.js"
+  },
+  {
+    "revision": "24742a2349a58af856ac",
+    "url": "/static/js/runtime-main.cf8b9ba7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
